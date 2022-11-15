532a674cde2027cc7ad84aac5a3c2eff01a941d3
Nico Ramirez <ramicaza@gmail.com>
2022-11-15 00:26:06 -0500

    contracts_patch.py working

--- cli/contracts_patch.py
+++ cli/contracts_patch.py
@@ -5,13 +5,33 @@ import pandas_gbq
 from google.oauth2 import service_account
 import os
 from polygonetl.cli import export_contracts
+from polygonetl.cli import extract_tokens
 import pandas as pd
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import traceback
+import json
+import argparse
+from tempfile import TemporaryDirectory
 
-# DATASET_NAME = 'public-data-finance.crypto_polygon'
-DATASET_NAME = 'bigquery-public-data.crypto_ethereum'
-PROJECT_ID = 'agave-pipeline'
-PROVIDER_URI = 'https://wild-restless-valley.quiknode.pro/c60f24249de4b4912bc95bbeb8f1cd6afe87588e/'
-OUTPUT_DIR = 'patch'
+
+def parse_date(d): return dt.datetime.strptime(d, '%Y%m%d').date()
+
+
+parser = argparse.ArgumentParser(description='Does big dump')
+parser.add_argument('--output-dir', '-o', type=str, required=True)
+parser.add_argument('--provider-uri', '-p', type=str, required=True)
+# 2015-08-07 is first eth contract date
+parser.add_argument('--start-date', '-s', type=parse_date, required=True)
+parser.add_argument('--end-date', '-e', type=parse_date, required=True)
+parser.add_argument('--dataset-name', '-d', type=str, required=True)
+parser.add_argument('--workers', '-w', type=int, default=32)
+args = parser.parse_args()
+
+# public-data-finance.crypto_polygon
+# bigquery-public-data.crypto_ethereum
+# 'https://wild-restless-valley.quiknode.pro/c60f24249de4b4912bc95bbeb8f1cd6afe87588e/'
+# 'http://54.187.135.32:8545'
 
 
 def setup_pandas_gbq(service_account_file='./service_account.json'):
@@ -26,34 +46,57 @@ def setup_pandas_gbq(service_account_file='./service_account.json'):
 def process_date(date: dt.date):
     sql = f"""
 SELECT address as contract_address, block_number
-FROM `{DATASET_NAME}.contracts`
+FROM `{args.dataset_name}.contracts`
 WHERE DATE(block_timestamp)='{date.strftime("%Y-%m-%d")}';
     """
     df = pandas_gbq.read_gbq(sql)
 
-    day_directory = os.path.join(OUTPUT_DIR, f'block_date={date.strftime("%Y-%m-%d")}')
-    os.mkdir(day_directory)
+    contracts_path = os.path.join(args.output_dir, 'contracts', f'{date.strftime("%Y-%m-%d")}.json')
+    with TemporaryDirectory() as tempdir:
+        addresses_path = os.path.join(tempdir, 'contract_addresses.csv')
+        df.to_csv(addresses_path)
 
-    addresses_path = os.path.join(day_directory, 'contract_addresses.csv')
-    df.to_csv(addresses_path)
-    export_contracts.callback(
-        batch_size=200,
-        receipts=addresses_path,
-        output=os.path.join(day_directory, "contracts.json"),
+        export_contracts.callback(
+            batch_size=200,
+            receipts=addresses_path,
+            output=contracts_path,
+            max_workers=5,
+            provider_uri=args.provider_uri,
+        )
+    
+    tokens_path = os.path.join(args.output_dir, 'tokens', f'{date.strftime("%Y-%m-%d")}.csv')
+    extract_tokens.callback(
+        contracts=contracts_path,
+        output=tokens_path,
         max_workers=5,
-        provider_uri=PROVIDER_URI,
+        provider_uri=args.provider_uri,
     )
-    os.remove(addresses_path)
+    
 
 
 def main():
     setup_pandas_gbq('./agave-pipeline-9ae294d3b02a.json')
-    for date in pd.date_range(dt.date(2015, 8, 7), dt.date(2022, 11, 9)):
-        # print(date.date())
-        process_date(date)
-    # test_date = dt.date(2022, 11, 6)
-    # 2015-08-07 is first eth contract date
-    # process_date(test_date)
+
+    date_range = list(pd.date_range(args.start_date, args.end_date))
+    with tqdm(total=len(date_range)) as pbar:
+        with ProcessPoolExecutor(max_workers=args.workers) as executor:
+            futures = {}
+            for date in date_range:
+                future = executor.submit(process_date, date)
+                futures[future] = date
+            exception_batches = []
+            for future in as_completed(futures):
+                date = futures[future]
+                try:
+                    result = future.result()
+                except Exception as e:
+                    print(f'Error processing batch {date}')
+                    exception_text = ''.join(traceback.format_exception(None, e, e.__traceback__))
+                    exception_batches.append({'batch': f'{date}', 'traceback': exception_text})
+                    with open(os.path.join(args.output_dir, 'exception_batches.json'), 'w') as fh:
+                        json.dump(exception_batches, fh)
+
+                pbar.update(1)
 
 
 if __name__ == '__main__':
